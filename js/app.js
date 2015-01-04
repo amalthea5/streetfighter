2ddb44d75cc9c9447e6395daaafd335a9431a3fe
amalthea5 <erin.knapp@gmail.com>
2015-01-04 12:40:04 -0500

    Keydown/Keyup Events

--- js/app.js
+++ js/app.js
@@ -27,6 +27,18 @@ $(document).ready(function() {
   })
 });
 
+$(document).keydown(function (e) {
+  if(e.keyCode == 88) {
+    $('.ryu-still').hide();
+    $('.ryu-cool').show();
+  }
+}).keyup(function (e) {
+  if (e.keyCode == 88) {
+    $('.ryu-cool').hide();
+    $('.ryu-still').show();
+  }
+});
+
 function playHadouken () {
 	$('#hadouken-sound') [0].volume = 0.5;
 	$('#hadouken-sound') [0].load();
